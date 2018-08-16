5b2b4a35f1502444009a1ad2519cf8ee355ebf10
Mr. Dragon <whyorean@gmail.com>
2018-08-17 04:26:46 +0530

    Fetch JSON to retrive app video details

--- app/src/main/java/com/dragons/aurora/fragment/details/Video.java
+++ app/src/main/java/com/dragons/aurora/fragment/details/Video.java
@@ -29,6 +29,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.bumptech.glide.request.RequestOptions;
@@ -36,20 +41,16 @@ import com.dragons.aurora.R;
 import com.dragons.aurora.fragment.DetailsFragment;
 import com.dragons.aurora.model.App;
 
+import org.json.JSONException;
+
+import static com.android.volley.VolleyLog.TAG;
+
 public class Video extends AbstractHelper {
+
+    private ImageView mImageView;
 
     public Video(DetailsFragment fragment, App app) {
         super(fragment, app);
-    }
-
-    private String getID(String URL) {
-        if (URL.contains("/youtu.be/"))
-            URL = URL.substring(URL.lastIndexOf('/') + 1, URL.length());
-        else if (URL.contains("feature"))
-            URL = URL.substring(URL.indexOf('=') + 1, URL.lastIndexOf('&'));
-        else
-            URL = URL.substring(URL.indexOf('=') + 1, URL.length());
-        return URL;
     }
 
     @Override
@@ -58,18 +59,8 @@ public class Video extends AbstractHelper {
             return;
         }
 
-        String vID = getID(app.getVideoUrl());
-        String URL = "https://img.youtube.com/vi/" + vID + "/hqdefault.jpg";
-
-        ImageView imageView = view.findViewById(R.id.thumbnail);
-
-        Glide
-                .with(context)
-                .load(URL)
-                .apply(new RequestOptions().centerCrop().placeholder(android.R.color.transparent))
-                .transition(new DrawableTransitionOptions().crossFade())
-                .into(imageView);
-
+        mImageView = view.findViewById(R.id.thumbnail);
+        getVideoThumbURL(app.getVideoUrl());
         view.findViewById(R.id.app_video).setVisibility(View.VISIBLE);
 
         ImageView play = view.findViewById(R.id.vid_play);
@@ -80,5 +71,25 @@ public class Video extends AbstractHelper {
                 Log.i(getClass().getSimpleName(), "Something is wrong with WebView");
             }
         });
+    }
+
+    private void getVideoThumbURL(String videoURL) {
+        RequestQueue mRequestQueue = Volley.newRequestQueue(context);
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                "http://www.youtube.com/oembed?url=" + videoURL + "&format=json",
+                null,
+                obj -> {
+                    try {
+                        Glide
+                                .with(context)
+                                .load(obj.getString("thumbnail_url"))
+                                .apply(new RequestOptions().centerCrop().placeholder(android.R.color.transparent))
+                                .transition(new DrawableTransitionOptions().crossFade())
+                                .into(mImageView);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }, error -> VolleyLog.d(TAG, "Error: " + error.getMessage()));
+        mRequestQueue.add(jsonObjReq);
     }
 }
