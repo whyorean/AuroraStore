d73a24a2b251e3dfb193da17f3c3a54f645de9d3
Mr. Dragon <whyorean@gmail.com>
2019-04-06 05:40:10 +0530

    Notifications : Do not show quick notifications if app notification are disable

--- app/src/main/java/com/aurora/store/notification/QuickNotification.java
+++ app/src/main/java/com/aurora/store/notification/QuickNotification.java
@@ -31,6 +31,7 @@ import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
 import com.aurora.store.R;
+import com.aurora.store.utility.NotificationUtil;
 
 public class QuickNotification extends NotificationBase {
 
@@ -50,31 +51,33 @@ public class QuickNotification extends NotificationBase {
     }
 
     public void show(String contentTitle, String contentText, PendingIntent contentIntent) {
-        manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        builder = new NotificationCompat.Builder(context, context.getPackageName())
-                .setAutoCancel(true)
-                .setCategory(NotificationCompat.CATEGORY_PROGRESS)
-                .setColorized(true)
-                .setColor(context.getResources().getColor(R.color.colorAccent))
-                .setContentTitle(contentTitle)
-                .setContentText(contentText)
-                .setOnlyAlertOnce(true)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setSmallIcon(R.drawable.ic_notification)
-                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+        if (NotificationUtil.isNotificationEnabled(context)) {
+            manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            builder = new NotificationCompat.Builder(context, context.getPackageName())
+                    .setAutoCancel(true)
+                    .setCategory(NotificationCompat.CATEGORY_PROGRESS)
+                    .setColorized(true)
+                    .setColor(context.getResources().getColor(R.color.colorAccent))
+                    .setContentTitle(contentTitle)
+                    .setContentText(contentText)
+                    .setOnlyAlertOnce(true)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setSmallIcon(R.drawable.ic_notification)
+                    .setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
 
-        if (contentIntent != null)
-            builder.setContentIntent(contentIntent);
+            if (contentIntent != null)
+                builder.setContentIntent(contentIntent);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            channel = new NotificationChannel(
-                    context.getPackageName(),
-                    context.getString(R.string.app_name),
-                    NotificationManager.IMPORTANCE_HIGH);
-            channel.setDescription("Aurora Store Quick Notification Channel");
-            manager.createNotificationChannel(channel);
-            builder.setChannelId(channel.getId());
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                channel = new NotificationChannel(
+                        context.getPackageName(),
+                        context.getString(R.string.app_name),
+                        NotificationManager.IMPORTANCE_HIGH);
+                channel.setDescription("Aurora Store Quick Notification Channel");
+                manager.createNotificationChannel(channel);
+                builder.setChannelId(channel.getId());
+            }
+            manager.notify(QUICK_NOTIFICATION_CHANNEL_ID, builder.build());
         }
-        manager.notify(QUICK_NOTIFICATION_CHANNEL_ID, builder.build());
     }
 }
