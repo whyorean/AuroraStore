4e7c6dba02b5b1e3f5c9d2c10feeadaec857c54a
Mr. Dragon <whyorean@gmail.com>
2019-07-31 02:05:25 +0530

    Installations : Add a cleanup routine to abandon installation sessions manually

--- app/src/main/java/com/aurora/store/fragment/preference/InstallationFragment.java
+++ app/src/main/java/com/aurora/store/fragment/preference/InstallationFragment.java
@@ -3,6 +3,7 @@ package com.aurora.store.fragment.preference;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInstaller;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,6 +16,8 @@ import androidx.preference.PreferenceFragmentCompat;
 import com.aurora.store.Constants;
 import com.aurora.store.R;
 import com.aurora.store.activity.SettingsActivity;
+import com.aurora.store.utility.ContextUtil;
+import com.aurora.store.utility.Log;
 import com.aurora.store.utility.PackageUtil;
 import com.aurora.store.utility.PathUtil;
 import com.aurora.store.utility.PrefUtil;
@@ -97,6 +100,23 @@ public class InstallationFragment extends PreferenceFragmentCompat implements Sh
             servicePreference.setEnabled(false);
             servicePreference.setSummary(getString(R.string.pref_services_desc_alt));
         }
+
+        Preference abandonPreference = findPreference(Constants.PREFERENCE_INSTALLATION_ABANDON_SESSION);
+        assert abandonPreference != null;
+        abandonPreference.setOnPreferenceClickListener(preference -> {
+            final PackageInstaller packageInstaller = context.getPackageManager().getPackageInstaller();
+            for (PackageInstaller.SessionInfo sessionInfo : packageInstaller.getMySessions()) {
+                final int sessionId = sessionInfo.getSessionId();
+                try {
+                    packageInstaller.abandonSession(sessionInfo.getSessionId());
+                    Log.e("Abandoned session id -> %d", sessionId);
+                } catch (Exception e) {
+                    Log.e("Unable to abandon session id ->", sessionId);
+                }
+            }
+            ContextUtil.toastLong(context, getString(R.string.toast_abandon_sessions));
+            return false;
+        });
     }
 
     @Override
