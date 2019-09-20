edba66bba52bae8eb4112473bb258d5b8cccda2f
Rahul Kumar Patel <whyorean@gmail.com>
2019-09-20 20:31:55 +0530

    Enforce proxy settings on Glide
    https://gitlab.com/AuroraOSS/AuroraStore/issues/173

--- app/src/main/java/com/aurora/store/AuroraGlide.java
+++ app/src/main/java/com/aurora/store/AuroraGlide.java
@@ -26,13 +26,21 @@ import android.graphics.Bitmap;
 import androidx.annotation.NonNull;
 
 import com.aurora.store.utility.Util;
+import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
 import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
 import com.bumptech.glide.load.engine.cache.LruResourceCache;
+import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
 import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.signature.ObjectKey;
+
+import java.io.InputStream;
+
+import okhttp3.OkHttpClient;
 
 import static com.bumptech.glide.load.DecodeFormat.PREFER_ARGB_8888;
 
@@ -50,11 +58,25 @@ public class AuroraGlide extends AppGlideModule {
                 .skipMemoryCache(false);
     }
 
+    private static OkHttpClient getOkHttpClient(Context context) {
+        final OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        if (Util.isNetworkProxyEnabled(context))
+            builder.proxy(Util.getNetworkProxy(context));
+        return builder.build();
+    }
+
     @Override
     public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
         int memoryCacheSizeBytes = 1024 * 1024 * 50;
         builder.setMemoryCache(new LruResourceCache(memoryCacheSizeBytes));
         builder.setDiskCache(new InternalCacheDiskCacheFactory(context, memoryCacheSizeBytes));
         builder.setDefaultRequestOptions(requestOptions(context));
+    }
+
+    @Override
+    public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
+        final OkHttpClient okHttpClient = getOkHttpClient(context);
+        final OkHttpUrlLoader.Factory okHttpUrlLoader = new OkHttpUrlLoader.Factory(okHttpClient);
+        registry.replace(GlideUrl.class, InputStream.class, okHttpUrlLoader);
     }
 }
