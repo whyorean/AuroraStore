1b6b459b1513319f338ea50d709386552714d6ec
Rahul Kumar Patel <whyorean@gmail.com>
2020-01-12 23:53:36 +0530

    Nooo, not again !

--- app/src/main/java/com/aurora/store/task/AppDetailTask.java
+++ app/src/main/java/com/aurora/store/task/AppDetailTask.java
@@ -24,7 +24,6 @@ import android.content.Context;
 
 import com.aurora.store.model.App;
 import com.aurora.store.model.AppBuilder;
-import com.aurora.store.util.Log;
 import com.aurora.store.util.PackageUtil;
 import com.dragons.aurora.playstoreapiv2.DetailsResponse;
 import com.dragons.aurora.playstoreapiv2.GooglePlayAPI;
@@ -42,7 +41,6 @@ public class AppDetailTask {
     public App getInfo(String packageName) throws Exception {
         DetailsResponse response = api.details(packageName);
         App app = AppBuilder.build(response);
-        Log.writeToFile(context,response);
         if (PackageUtil.isInstalled(context, app))
             app.setInstalled(true);
         return app;
