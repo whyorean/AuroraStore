7c71ca24ecb06dab88ae26e00f0abb6738db885c
Rahul Kumar Patel <whyorean@gmail.com>
2020-04-14 13:01:35 +0530

    Fix auto updates check

--- app/src/main/java/com/aurora/store/receiver/UpdatesReceiver.java
+++ app/src/main/java/com/aurora/store/receiver/UpdatesReceiver.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import com.aurora.store.AuroraApplication;
 import com.aurora.store.Constants;
 import com.aurora.store.R;
+import com.aurora.store.api.PlayStoreApiAuthenticator;
 import com.aurora.store.model.App;
 import com.aurora.store.notification.QuickNotification;
 import com.aurora.store.task.LiveUpdate;
@@ -15,15 +16,14 @@ import com.aurora.store.task.ObservableDeliveryData;
 import com.aurora.store.task.UpdatableAppsTask;
 import com.aurora.store.ui.main.AuroraActivity;
 import com.aurora.store.util.Log;
+import com.aurora.store.util.TextUtil;
 import com.aurora.store.util.Util;
-import com.dragons.aurora.playstoreapiv2.GooglePlayAPI;
 
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
 import io.reactivex.Observable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
 
@@ -36,57 +36,49 @@ public class UpdatesReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         this.context = context;
         Log.i("Update check Started");
-        CompositeDisposable disposable = new CompositeDisposable();
-        UpdatableAppsTask updatableAppTask = new UpdatableAppsTask(new GooglePlayAPI(), context);
-        disposable.add(Observable.fromCallable(updatableAppTask::getUpdatableApps)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe((appList) -> {
-                    StringBuilder msg;
-                    if (!appList.isEmpty()) {
-                        msg = new StringBuilder()
-                                .append(appList.size())
-                                .append(StringUtils.SPACE)
-                                .append(context.getString(R.string.list_update_all_txt));
 
+        disposable.add(Observable.fromCallable(() -> new PlayStoreApiAuthenticator().getPlayApi(context))
+                .subscribeOn(Schedulers.io())
+                .map(api -> new UpdatableAppsTask(AuroraApplication.api, context).getUpdatableApps())
+                .subscribe(apps -> {
+                    if (apps.isEmpty()) {
+                        Log.e(context.getString(R.string.list_empty_updates));
+                    } else {
                         QuickNotification.show(context,
                                 context.getString(R.string.action_updates),
-                                msg.toString(),
+                                StringUtils.joinWith(StringUtils.SPACE, apps.size(), context.getString(R.string.list_update_all_txt)),
                                 getContentIntent(context));
+
                         if (Util.isAutoUpdatesEnabled(context))
-                            updateAllApps(appList);
+                            updateAllApps(apps);
                     }
-                }, err -> Log.e("Update check failed")));
+                }, throwable -> Log.e("Update check failed : %s", throwable.getMessage())));
     }
 
     private void updateAllApps(List<App> updatableAppList) {
         AuroraApplication.setOngoingUpdateList(updatableAppList);
         AuroraApplication.setBulkUpdateAlive(true);
+
+        QuickNotification.show(context,
+                context.getString(R.string.action_updates),
+                context.getString(R.string.list_updating_background),
+                getContentIntent(context));
+
         disposable.add(Observable.fromIterable(updatableAppList)
-                .flatMap(app -> new ObservableDeliveryData(context).getDeliveryData(app))
                 .subscribeOn(Schedulers.io())
-                .doOnSubscribe(disposable -> {
-                    QuickNotification.show(context,
-                            context.getString(R.string.action_updates),
-                            context.getString(R.string.list_updating_background),
-                            getContentIntent(context));
-                })
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnNext(deliveryDataBundle -> new LiveUpdate(context)
-                        .enqueueUpdate(deliveryDataBundle.getApp(),
-                                deliveryDataBundle.getAndroidAppDeliveryData()))
+                .flatMap(app -> new ObservableDeliveryData(context).getDeliveryData(app))
+                .doOnNext(bundle -> new LiveUpdate(context).enqueueUpdate(bundle.getApp(), bundle.getAndroidAppDeliveryData()))
                 .doOnError(err -> {
                     QuickNotification.show(context,
                             context.getString(R.string.list_updating_failed),
-                            err.getMessage() != null ? err.getMessage() : "",
+                            TextUtil.emptyIfNull(err.getMessage()),
                             getContentIntent(context));
                 })
                 .subscribe());
     }
 
-
     private PendingIntent getContentIntent(Context context) {
-        Intent intent = new Intent(context, AuroraActivity.class);
+        final Intent intent = new Intent(context, AuroraActivity.class);
         intent.putExtra(Constants.INTENT_FRAGMENT_POSITION, 1);
         return PendingIntent.getActivity(context, 0, intent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
