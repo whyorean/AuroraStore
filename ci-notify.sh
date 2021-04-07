5d5b2e5db4a201582b411a41d81273f0f37456c6
marchingon12 <augustthegreat.cool@gmail.com>
2021-04-07 15:48:27 +0200

    use correct name and formatting

--- ci-notify.sh
+++ ci-notify.sh
@@ -10,24 +10,23 @@ TEXT="Project:+$CI_PROJECT_NAME<a href=$CI_PIPELINE_URL><b>Building<b><a> with H
 #curl -s --max-time $TIME -d "chat_id=$TELEGRAM_CHAT_ID&disable_web_page_preview=1&text=$TEXT" $msgURL > /dev/null
 
 curl -s --max-time $TIME $msgURL -d chat_id=$TELEGRAM_CHAT_ID \
-     -d "disable_web_page_preview=true" \
-     -d "parse_mode=html" \
-     -d text=$TEXT
+    -d "disable_web_page_preview=true" \
+    -d "parse_mode=html" \
+    -d text=$TEXT
 
-
-if [ -f app/bappld/outputs/apk/debug/app-release.apk ]; then
-    cp app/bappld/outputs/apk/debug/app-release.apk app/build/outputs/apk/debug/AuroraStore_$DATE.apk
+if [ -f app/bappld/outputs/apk/debug/app-debug.apk ]; then
+    cp app/bappld/outputs/apk/debug/app-debug.apk app/build/outputs/apk/debug/AuroraStore_$DATE.apk
     cd app/build/outputs/apk/debug/
     APK=$(echo AuroraStore_*.apk)
     curl -F document=@"$APK" $docURL \
-         -F chat_id=$TELEGRAM_CHAT_ID  \
-         -F "disable_web_page_preview=true" \
-         -F "parse_mode=html" \
-         -F caption="✅ <b>CI build completed successfully!</b>"
+        -F chat_id=$TELEGRAM_CHAT_ID \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="✅ <b>CI build completed successfully!</b>"
     rm -rf ui/build/outputs/apk
 else
     curl -s -X POST $msgURL -d chat_id=$TELEGRAM_CHAT_ID \
-         -d "disable_web_page_preview=true" \
-         -d "parse_mode=html" \
-         -d text="❌ <b>Build error, exiting now!</b>"
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="❌ <b>Build error, exiting now!</b>"
 fi
