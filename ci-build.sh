d8255d6dcc6548b274fdcd8ae50453c42a5c1d72
marchingon12 <augustthegreat.cool@gmail.com>
2021-04-08 01:00:42 +0200

    CI: try in markdownv2 mode

--- ci-build.sh
+++ ci-build.sh
@@ -3,9 +3,9 @@
 MSG_URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
 DOC_URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendDocument"
 COMMIT_URL="https://gitlab.com/AuroraOSS/AuroraStore/-/commit/$CI_COMMIT_SHA"
-TEXT="Project:+$CI_PROJECT_NAME%0A<a+href=%22$CI_PIPELINE_URL%22><b>Building<b><a>+with+HEAD+at+<a+href=%22$COMMIT_URL%22><b>$CI_COMMIT_SHORT_SHA<b><a>+by+$CI_COMMIT_AUTHOR.%0ABranch:+$CI_COMMIT_BRANCH"
+TEXT="Project: $CI_PROJECT_NAME%0A[*Building*]($CI_PIPELINE_URL) with HEAD at [*$CI_COMMIT_SHORT_SHA*]($COMMIT_URL) by $CI_COMMIT_AUTHOR%0ABranch: $CI_COMMIT_BRANCH"
 
 curl -s -X POST $MSG_URL -d chat_id=$TELEGRAM_CHAT_ID \
     -d "disable_web_page_preview=true" \
-    -d "parse_mode=HTML" \
+    -d "parse_mode=MarkdownV2" \
     -d text=$TEXT
