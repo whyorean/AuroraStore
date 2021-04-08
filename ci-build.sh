98f78a729d79d11f845723af4a02a1922e05ca39
marchingon12 <augustthegreat.cool@gmail.com>
2021-04-08 11:02:09 +0200

    try again

--- ci-build.sh
+++ ci-build.sh
@@ -4,9 +4,9 @@ MSG_URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
 DOC_URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendDocument"
 COMMIT_URL="https://gitlab.com/AuroraOSS/AuroraStore/-/commit/$CI_COMMIT_SHA"
 #TEXT="Project:+[${CI_PROJECT_NAME}](${CI_PROJECT_URL})%0A[*⚒️+Building*](${CI_JOB_URL})+with+HEAD+at+[*${CI_COMMIT_SHORT_SHA}*](${COMMIT_URL})+by+${CI_COMMIT_AUTHOR}"
-TEXT="Project:+<a+href=${CI_PROJECT_URL}>${CI_PROJECT_NAME}</a>%0A<a+href=${CI_JOB_URL}><b>⚒️+Building</b></a>+with+HEAD+at+<a+href=${COMMIT_URL}><b>${CI_COMMIT_SHORT_SHA}</b></a>+by+${CI_COMMIT_AUTHOR}"
+
 
 curl -s -X POST $MSG_URL -d chat_id=$TELEGRAM_CHAT_ID \
     -d "disable_web_page_preview=true" \
     -d "parse_mode=HTML" \
-    -d text=$TEXT
+    -d text=Project:+<a+href=${CI_PROJECT_URL}>${CI_PROJECT_NAME}</a>%0A<a+href=${CI_JOB_URL}><b>⚒️+Building</b></a>+with+HEAD+at+<a+href=${COMMIT_URL}><b>${CI_COMMIT_SHORT_SHA}</b></a>+by+${CI_COMMIT_AUTHOR}
