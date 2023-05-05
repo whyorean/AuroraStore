0bef2e3da7508ca527cc2ddfb5629297ab3fa10d
Aayush Gupta <aayushgupta219@gmail.com>
2023-05-05 10:08:37 +0530

    fixup! AuthViewModel: Search in custom tab by default in anonymous accounts

--- app/src/main/java/com/aurora/store/view/ui/commons/BaseActivity.kt
+++ app/src/main/java/com/aurora/store/view/ui/commons/BaseActivity.kt
@@ -133,6 +133,7 @@ abstract class BaseActivity : AppCompatActivity(), NetworkProvider.NetworkListen
     }
 
     fun openGoogleActivity() {
+        Preferences.putBoolean(this, Preferences.PREFERENCE_ADVANCED_SEARCH_IN_CTT, false)
         val intent = Intent(this, GoogleActivity::class.java)
         startActivity(
             intent,
