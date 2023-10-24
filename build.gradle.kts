5cc49daa1279e3db5bf5d96ed3d7136e77abd5b4
Aayush Gupta <aayushgupta219@gmail.com>
2023-10-25 00:14:34 +0530

    Bump to latest stable AGP and Gradle version
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@
  */
 
 plugins {
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.10" apply false
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
