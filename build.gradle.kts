2d8de93ebd3787108ee7b1aaaac118b636b4d3b4
Aayush Gupta <aayushgupta219@gmail.com>
2023-12-02 14:46:16 +0530

    Bump AGP to 8.2.0
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@
  */
 
 plugins {
-    id("com.android.application") version "8.1.4" apply false
+    id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.20" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
