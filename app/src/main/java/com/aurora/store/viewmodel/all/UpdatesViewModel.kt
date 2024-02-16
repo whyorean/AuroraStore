b35f6e0f1d37a8d2ca84472fc5ba0bd223aa6624
Aayush Gupta <aayushgupta219@gmail.com>
2024-02-16 18:19:48 +0530

    UpdatesViewModel: Only cancel updates with cancelAll button
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/com/aurora/store/viewmodel/all/UpdatesViewModel.kt
+++ app/src/main/java/com/aurora/store/viewmodel/all/UpdatesViewModel.kt
@@ -96,6 +96,6 @@ class UpdatesViewModel @Inject constructor(
     }
 
     fun cancelAll() {
-        viewModelScope.launch { downloadWorkerUtil.cancelAll() }
+        viewModelScope.launch { downloadWorkerUtil.cancelAll(downloads = false) }
     }
 }
