687b8f0e686593f2b531ad250a7bc31b0f8c6b60
Aayush Gupta <aayushgupta219@gmail.com>
2024-04-04 13:46:39 +0530

    Constants: Drop unused variables
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/com/aurora/Constants.kt
+++ app/src/main/java/com/aurora/Constants.kt
@@ -20,22 +20,14 @@
 package com.aurora
 
 object Constants {
-    const val INT_EXTRA = "INT_EXTRA"
-    const val FLOAT_EXTRA = "FLOAT_EXTRA"
-    const val STRING_APP = "STRING_APP"
-    const val STRING_VERSION = "STRING_VERSION"
-    const val PARCEL_DOWNLOAD = "PARCEL_DOWNLOAD"
-    const val STRING_EXTRA = "STRING_EXTRA"
-    const val BROWSE_EXTRA = "BROWSE_EXTRA"
 
-    const val FETCH_GROUP_ID = "FETCH_GROUP_ID"
+    const val PARCEL_DOWNLOAD = "PARCEL_DOWNLOAD"
+
     const val APP_ID = "com.aurora.store"
 
     const val EXODUS_SUBMIT_PAGE = "https://reports.exodus-privacy.eu.org/analysis/submit/#"
-    const val EXODUS_BASE_URL = "https://reports.exodus-privacy.eu.org/api/search/"
     const val EXODUS_REPORT_URL = "https://reports.exodus-privacy.eu.org/reports/"
     const val SHARE_URL = "https://play.google.com/store/apps/details?id="
-    const val TOS_URL = "https://gitlab.com/AuroraOSS/AuroraStore/-/blob/master/TermsOfService.md"
     const val UPDATE_URL = "https://gitlab.com/AuroraOSS/AuroraStore/raw/master/updates.json"
 
     const val NOTIFICATION_CHANNEL_ALERT = "NOTIFICATION_CHANNEL_ALERT"
@@ -46,7 +38,6 @@ object Constants {
     const val GITLAB_URL = "https://gitlab.com/AuroraOSS/AuroraStore"
     const val URL_DISPENSER = "https://auroraoss.com/api/auth"
 
-    const val PLAY_QUERY_URL = "https://play.google.com/store/search?q="
     const val ANDROID_CONNECTIVITY_URL = "https://connectivitycheck.android.com/generate_204"
 
     //ACCOUNTS
