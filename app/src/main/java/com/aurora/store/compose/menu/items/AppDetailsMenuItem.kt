cd6999158046390662d28fe302c31a45d63dc402
Aayush Gupta <aayushgupta219@gmail.com>
2025-05-16 17:49:38 +0800

    compose: details: Setup menu
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

---
+++ app/src/main/java/com/aurora/store/compose/menu/items/AppDetailsMenuItem.kt
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The Calyx Institute
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package com.aurora.store.compose.menu.items
+
+/**
+ * Valid menu items for app details screen for handling clicks
+ */
+enum class AppDetailsMenuItem {
+    FAVORITE,
+    SHARE,
+    MANUAL_DOWNLOAD,
+    APP_INFO,
+    PLAY_STORE,
+    ADD_TO_HOME
+}
