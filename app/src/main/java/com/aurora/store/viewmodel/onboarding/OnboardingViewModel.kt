dee01c8e03a26f4e0f5b60bcb3c487dfb3f7a96e
Rahul Patel <whyorean@gmail.com>
2025-05-24 00:24:25 +0530

    Allow huawei variants to install microG

--- app/src/main/java/com/aurora/store/viewmodel/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/com/aurora/store/viewmodel/onboarding/OnboardingViewModel.kt
@@ -6,9 +6,23 @@
 package com.aurora.store.viewmodel.onboarding
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.aurora.store.data.helper.UpdateHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class OnboardingViewModel @Inject constructor(val updateHelper: UpdateHelper) : ViewModel()
+class OnboardingViewModel @Inject constructor(val updateHelper: UpdateHelper) : ViewModel() {
+
+    private val _page = MutableStateFlow<OnboardingPage>(OnboardingPage.WELCOME)
+    val currentPage: StateFlow<OnboardingPage> = _page
+
+    fun setCurrentPage(page: OnboardingPage) {
+        viewModelScope.launch {
+            _page.emit(page)
+        }
+    }
+}
