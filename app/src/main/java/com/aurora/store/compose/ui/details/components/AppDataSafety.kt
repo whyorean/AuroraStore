b720f0154db74413141a780a945944afb80e4896
Aayush Gupta <aayushgupta219@gmail.com>
2025-06-27 13:50:10 +0800

    compose: Relocate preview to top directory
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/com/aurora/store/compose/ui/details/components/AppDataSafety.kt
+++ app/src/main/java/com/aurora/store/compose/ui/details/components/AppDataSafety.kt
@@ -22,7 +22,7 @@ import com.aurora.gplayapi.data.models.datasafety.Report
 import com.aurora.store.R
 import com.aurora.store.compose.composables.HeaderComposable
 import com.aurora.store.compose.composables.InfoComposable
-import com.aurora.store.compose.composables.preview.AppPreviewProvider
+import com.aurora.store.compose.preview.AppPreviewProvider
 
 /**
  * Composable to display app's data safety report, supposed to be used as a part
