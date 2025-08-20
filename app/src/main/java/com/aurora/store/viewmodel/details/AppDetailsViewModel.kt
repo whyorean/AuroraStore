JHAID-DEV/JStore

compose: Initial migration of AppDetails* logic to compose [2/*]
Deprecate setting to hide similar and related apps as they will be always
listed in the suggestions pane on widescreen devices

Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

app/src/main/java/com/aurora/store/viewmodel/details/AppDetailsViewModel.kt
@@ -7,11 +7,10 @@
 package com.aurora.store.viewmodel.details
 
 import android.content.Context
+import android.content.pm.PackageInstaller
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
-import androidx.paging.cachedIn
 import com.aurora.Constants
 import com.aurora.gplayapi.data.models.App
 import com.aurora.gplayapi.data.models.Review
@@ -24,11 +23,9 @@ import com.aurora.gplayapi.network.IHttpClient
 import com.aurora.store.BuildConfig
 import com.aurora.store.data.helper.DownloadHelper
 import com.aurora.store.data.model.ExodusReport
-import com.aurora.store.data.model.ExodusTracker
 import com.aurora.store.data.model.PlexusReport
 import com.aurora.store.data.model.Report
 import com.aurora.store.data.model.Scores
-import com.aurora.store.data.paging.GenericPagingSource.Companion.createPager
 import com.aurora.store.data.providers.AuthProvider
 import com.aurora.store.data.room.favourite.Favourite
 import com.aurora.store.data.room.favourite.FavouriteDao
@@ -36,21 +33,19 @@ import com.aurora.store.util.CertUtil
 import com.aurora.store.util.PackageUtil
 import com.aurora.store.util.PackageUtil.PACKAGE_NAME_GMS
 import com.aurora.store.util.Preferences
-import com.aurora.store.util.Preferences.PREFERENCE_SIMILAR
 import com.aurora.store.util.Preferences.PREFERENCE_UPDATES_EXTENDED
 import com.google.gson.Gson
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import org.json.JSONObject
@@ -68,8 +63,7 @@ class AppDetailsViewModel @Inject constructor(
     private val downloadHelper: DownloadHelper,
     private val favouriteDao: FavouriteDao,
     private val httpClient: IHttpClient,
-    private val gson: Gson,
-    private val exodusTrackers: JSONObject
+    private val gson: Gson
 ) : ViewModel() {
 
     private val TAG = AppDetailsViewModel::class.java.simpleName
@@ -77,9 +71,8 @@ class AppDetailsViewModel @Inject constructor(
     private val _app = MutableStateFlow<App?>(App(""))
     val app = _app.asStateFlow()
 
-    private var reviewsNextPageUrl: String? = null
-    private val _reviews = MutableStateFlow<PagingData<Review>>(PagingData.empty())
-    val reviews = _reviews.asStateFlow()
+    private val _suggestions = MutableStateFlow<List<App>>(emptyList())
+    val suggestions = _suggestions.asStateFlow()
 
     private val _userReview = MutableStateFlow<Review?>(null)
     val userReview = _userReview.asStateFlow()
@@ -107,6 +100,27 @@ class AppDetailsViewModel @Inject constructor(
         list.find { d -> d.packageName == a?.packageName }
     }.stateIn(viewModelScope, SharingStarted.Eagerly, null)
 
+    // TODO: Save sessionId to downloads to monitor packageInstall progress
+    val installProgress = callbackFlow<Float?> {
+        val packageInstaller = context.packageManager.packageInstaller
+        val callback = object : PackageInstaller.SessionCallback() {
+            override fun onActiveChanged(p0: Int, p1: Boolean) {}
+            override fun onBadgingChanged(p0: Int) {}
+            override fun onCreated(p0: Int) {
+                // trySend(0F)
+            }
+            override fun onFinished(p0: Int, p1: Boolean) {
+//                trySend(null)
+            }
+            override fun onProgressChanged(p0: Int, p1: Float) {
+//                trySend(p1)
+            }
+        }
+
+        packageInstaller.registerSessionCallback(callback)
+        awaitClose { packageInstaller.unregisterSessionCallback(callback) }
+    }.stateIn(viewModelScope, SharingStarted.Eagerly, null)
+
     private val isExtendedUpdateEnabled: Boolean
         get() = Preferences.getBoolean(context, PREFERENCE_UPDATES_EXTENDED)
 
@@ -128,15 +142,9 @@ class AppDetailsViewModel @Inject constructor(
     val hasValidUpdate: Boolean
         get() = (isUpdatable && hasValidCerts) || (isUpdatable && isExtendedUpdateEnabled)
 
-    val showSimilarApps: Boolean
-        get() = Preferences.getBoolean(context, PREFERENCE_SIMILAR)
-
     fun fetchAppDetails(packageName: String) {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                _favourite.value = favouriteDao.isFavourite(packageName)
-
-                _dataSafetyReport.value = webDataSafetyHelper.fetch(packageName)
                 _app.value = appDetailsHelper.getAppByPackageName(packageName).apply {
                     isInstalled = PackageUtil.isInstalled(context, packageName)
                 }
@@ -148,7 +156,9 @@ class AppDetailsViewModel @Inject constructor(
             // Only proceed if there was no error while fetching the app details
             if (throwable != null) return@invokeOnCompletion
 
-            fetchReviews()
+            fetchFavourite(packageName)
+            fetchDataSafetyReport(packageName)
+            fetchSuggestions()
             fetchExodusPrivacyReport(packageName)
             if (app.value!!.dependencies.dependentPackages.contains(PACKAGE_NAME_GMS)) {
                 fetchPlexusReport(packageName)
@@ -156,48 +166,11 @@ class AppDetailsViewModel @Inject constructor(
         }
     }
 
-    fun fetchReviews(filter: Review.Filter = Review.Filter.ALL) {
-        reviewsNextPageUrl = null
-
-        createPager {
-            val reviewsCluster = reviewsNextPageUrl?.let { nextPageUrl ->
-                reviewsHelper.next(nextPageUrl)
-            } ?: reviewsHelper.getReviews(app.value!!.packageName, filter)
-
-            reviewsNextPageUrl = reviewsCluster.nextPageUrl
-            reviewsCluster.reviewList
-        }.flow.distinctUntilChanged()
-            .cachedIn(viewModelScope)
-            .onEach { _reviews.value = it }
-            .launchIn(viewModelScope)
-    }
-
-    private fun fetchExodusPrivacyReport(packageName: String) {
+    fun updateTestingProgramStatus(packageName: String, subscribe: Boolean) {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                _exodusReport.value = getLatestExodusReport(packageName)
-            } catch (exception: Exception) {
-                Log.e(TAG, "Failed to fetch privacy report", exception)
-                _exodusReport.value = null
-            }
-        }
-    }
-
-    fun fetchPlexusReport(packageName: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            try {
-                _plexusScores.value = getPlexusReport(packageName)?.report?.scores
-            } catch (exception: Exception) {
-                Log.e(TAG, "Failed to fetch compatibility report", exception)
-                _plexusScores.value = null
-            }
-        }
-    }
-
-    fun fetchTestingProgramStatus(packageName: String, subscribe: Boolean) {
-        viewModelScope.launch(Dispatchers.IO) {
-            try {
-                _testingProgramStatus.value = appDetailsHelper.testingProgram(packageName, subscribe)
+                _testingProgramStatus.value =
+                    appDetailsHelper.testingProgram(packageName, subscribe)
             } catch (exception: Exception) {
                 Log.e(TAG, "Failed to fetch testing program status", exception)
             }
@@ -272,24 +245,50 @@ class AppDetailsViewModel @Inject constructor(
         }
     }
 
-    fun getExodusTrackersFromReport(): List<ExodusTracker> {
-        val trackerObjects = exodusReport.value!!.trackers.map {
-            exodusTrackers.getJSONObject(it.toString())
-        }.toList()
+    private fun fetchFavourite(packageName: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _favourite.value = favouriteDao.isFavourite(packageName)
+        }
+    }
 
-        return trackerObjects.map {
-            ExodusTracker(
-                id = it.getInt("id"),
-                name = it.getString("name"),
-                url = it.getString("website"),
-                signature = it.getString("code_signature"),
-                date = it.getString("creation_date"),
-                description = it.getString("description"),
-                networkSignature = it.getString("network_signature"),
-                documentation = listOf(it.getString("documentation")),
-                categories = listOf(it.getString("categories"))
-            )
-        }.toList()
+    private fun fetchDataSafetyReport(packageName: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _dataSafetyReport.value = webDataSafetyHelper.fetch(packageName)
+        }
+    }
+
+    private fun fetchSuggestions() {
+        // Bail out if we go no suggestions to offer
+        if (app.value!!.detailsStreamUrl.isNullOrBlank()) return
+
+        viewModelScope.launch(Dispatchers.IO) {
+            val streamBundle = appDetailsHelper.getDetailsStream(app.value!!.detailsStreamUrl!!)
+            _suggestions.value = streamBundle.streamClusters.values
+                .flatMap { it.clusterAppList }
+                .distinctBy { it.packageName }
+        }
+    }
+
+    private fun fetchExodusPrivacyReport(packageName: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                _exodusReport.value = getLatestExodusReport(packageName)
+            } catch (exception: Exception) {
+                Log.e(TAG, "Failed to fetch privacy report", exception)
+                _exodusReport.value = null
+            }
+        }
+    }
+
+    private fun fetchPlexusReport(packageName: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                _plexusScores.value = getPlexusReport(packageName)?.report?.scores
+            } catch (exception: Exception) {
+                Log.e(TAG, "Failed to fetch compatibility report", exception)
+                _plexusScores.value = null
+            }
+        }
     }
 
     private fun getLatestExodusReport(packageName: String): Report? {
@@ -305,12 +304,6 @@ class AppDetailsViewModel @Inject constructor(
             .firstOrNull()
     }
 
-    private fun getPlexusReport(packageName: String): PlexusReport? {
-        val url = "${Constants.PLEXUS_API_URL}/${packageName}/?scores=true"
-        val playResponse = httpClient.get(url, emptyMap())
-        return gson.fromJson(String(playResponse.responseBytes), PlexusReport::class.java)
-    }
-
     private fun parseExodusResponse(response: String, packageName: String): List<Report> {
         try {
             val jsonObject = JSONObject(response)
@@ -323,5 +316,11 @@ class AppDetailsViewModel @Inject constructor(
         } catch (e: Exception) {
             return emptyList()
         }
+    }
+
+    private fun getPlexusReport(packageName: String): PlexusReport? {
+        val url = "${Constants.PLEXUS_API_URL}/${packageName}/?scores=true"
+        val playResponse = httpClient.get(url, emptyMap())
+        return gson.fromJson(String(playResponse.responseBytes), PlexusReport::class.java)
     }
 }

whyorean/AuroraStore

app/src/main/java/com/aurora/store/viewmodel/details/AppDetailsViewModel.kt
@@ -7,11 +7,10 @@
 package com.aurora.store.viewmodel.details
 
 import android.content.Context
+import android.content.pm.PackageInstaller
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
-import androidx.paging.cachedIn
 import com.aurora.Constants
 import com.aurora.gplayapi.data.models.App
 import com.aurora.gplayapi.data.models.Review
@@ -24,11 +23,9 @@ import com.aurora.gplayapi.network.IHttpClient
 import com.aurora.store.BuildConfig
 import com.aurora.store.data.helper.DownloadHelper
 import com.aurora.store.data.model.ExodusReport
-import com.aurora.store.data.model.ExodusTracker
 import com.aurora.store.data.model.PlexusReport
 import com.aurora.store.data.model.Report
 import com.aurora.store.data.model.Scores
-import com.aurora.store.data.paging.GenericPagingSource.Companion.createPager
 import com.aurora.store.data.providers.AuthProvider
 import com.aurora.store.data.room.favourite.Favourite
 import com.aurora.store.data.room.favourite.FavouriteDao
@@ -36,20 +33,18 @@ import com.aurora.store.util.CertUtil
 import com.aurora.store.util.PackageUtil
 import com.aurora.store.util.PackageUtil.PACKAGE_NAME_GMS
 import com.aurora.store.util.Preferences
-import com.aurora.store.util.Preferences.PREFERENCE_SIMILAR
 import com.aurora.store.util.Preferences.PREFERENCE_UPDATES_EXTENDED
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
@@ -68,8 +63,7 @@ class AppDetailsViewModel @Inject constructor(
     private val downloadHelper: DownloadHelper,
     private val favouriteDao: FavouriteDao,
     private val httpClient: IHttpClient,
-    private val json: Json,
-    private val exodusTrackers: JSONObject
+    private val json: Json
 ) : ViewModel() {
 
     private val TAG = AppDetailsViewModel::class.java.simpleName
@@ -77,9 +71,8 @@ class AppDetailsViewModel @Inject constructor(
     private val _app = MutableStateFlow<App?>(App(""))
     val app = _app.asStateFlow()
 
-    private var reviewsNextPageUrl: String? = null
-    private val _reviews = MutableStateFlow<PagingData<Review>>(PagingData.empty())
-    val reviews = _reviews.asStateFlow()
+    private val _suggestions = MutableStateFlow<List<App>>(emptyList())
+    val suggestions = _suggestions.asStateFlow()
 
     private val _userReview = MutableStateFlow<Review?>(null)
     val userReview = _userReview.asStateFlow()
@@ -107,6 +100,27 @@ class AppDetailsViewModel @Inject constructor(
         list.find { d -> d.packageName == a?.packageName }
     }.stateIn(viewModelScope, SharingStarted.Eagerly, null)
 
+    // TODO: Save sessionId to downloads to monitor packageInstall progress
+    val installProgress = callbackFlow<Float?> {
+        val packageInstaller = context.packageManager.packageInstaller
+        val callback = object : PackageInstaller.SessionCallback() {
+            override fun onActiveChanged(p0: Int, p1: Boolean) {}
+            override fun onBadgingChanged(p0: Int) {}
+            override fun onCreated(p0: Int) {
+                // trySend(0F)
+            }
+            override fun onFinished(p0: Int, p1: Boolean) {
+//                trySend(null)
+            }
+            override fun onProgressChanged(p0: Int, p1: Float) {
+//                trySend(p1)
+            }
+        }
+
+        packageInstaller.registerSessionCallback(callback)
+        awaitClose { packageInstaller.unregisterSessionCallback(callback) }
+    }.stateIn(viewModelScope, SharingStarted.Eagerly, null)
+
     private val isExtendedUpdateEnabled: Boolean
         get() = Preferences.getBoolean(context, PREFERENCE_UPDATES_EXTENDED)
 
@@ -114,7 +128,7 @@ class AppDetailsViewModel @Inject constructor(
         get() = PackageUtil.isUpdatable(
             context,
             app.value!!.packageName,
-            app.value!!.versionCode.toLong()
+            app.value!!.versionCode
         )
 
     private val hasValidCerts: Boolean
@@ -128,15 +142,9 @@ class AppDetailsViewModel @Inject constructor(
     val hasValidUpdate: Boolean
         get() = (isUpdatable && hasValidCerts) || (isUpdatable && isExtendedUpdateEnabled)
 
-    val showSimilarApps: Boolean
-        get() = Preferences.getBoolean(context, PREFERENCE_SIMILAR)
-
     fun fetchAppDetails(packageName: String) {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                _favourite.value = favouriteDao.isFavourite(packageName)
-
-                _dataSafetyReport.value = webDataSafetyHelper.fetch(packageName)
                 _app.value = appDetailsHelper.getAppByPackageName(packageName).copy(
                     isInstalled = PackageUtil.isInstalled(context, packageName)
                 )
@@ -148,7 +156,9 @@ class AppDetailsViewModel @Inject constructor(
             // Only proceed if there was no error while fetching the app details
             if (throwable != null) return@invokeOnCompletion
 
-            fetchReviews()
+            fetchFavourite(packageName)
+            fetchDataSafetyReport(packageName)
+            fetchSuggestions()
             fetchExodusPrivacyReport(packageName)
             if (app.value!!.dependencies.dependentPackages.contains(PACKAGE_NAME_GMS)) {
                 fetchPlexusReport(packageName)
@@ -156,48 +166,11 @@ class AppDetailsViewModel @Inject constructor(
         }
     }
 
-    fun fetchReviews(filter: Review.Filter = Review.Filter.ALL) {
-        reviewsNextPageUrl = null
-
-        createPager {
-            val reviewsCluster = reviewsNextPageUrl?.let { nextPageUrl ->
-                reviewsHelper.next(nextPageUrl)
-            } ?: reviewsHelper.getReviews(app.value!!.packageName, filter)
-
-            reviewsNextPageUrl = reviewsCluster.nextPageUrl
-            reviewsCluster.reviewList
-        }.flow.distinctUntilChanged()
-            .cachedIn(viewModelScope)
-            .onEach { _reviews.value = it }
-            .launchIn(viewModelScope)
-    }
-
-    private fun fetchExodusPrivacyReport(packageName: String) {
+    fun updateTestingProgramStatus(packageName: String, subscribe: Boolean) {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                _exodusReport.value = getLatestExodusReport(packageName)
-            } catch (exception: Exception) {
-                Log.e(TAG, "Failed to fetch privacy report", exception)
-                _exodusReport.value = null
-            }
-        }
-    }
-
-    fun fetchPlexusReport(packageName: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            try {
-                _plexusScores.value = getPlexusReport(packageName)?.report?.scores
-            } catch (exception: Exception) {
-                Log.e(TAG, "Failed to fetch compatibility report", exception)
-                _plexusScores.value = null
-            }
-        }
-    }
-
-    fun fetchTestingProgramStatus(packageName: String, subscribe: Boolean) {
-        viewModelScope.launch(Dispatchers.IO) {
-            try {
-                _testingProgramStatus.value = appDetailsHelper.testingProgram(packageName, subscribe)
+                _testingProgramStatus.value =
+                    appDetailsHelper.testingProgram(packageName, subscribe)
             } catch (exception: Exception) {
                 Log.e(TAG, "Failed to fetch testing program status", exception)
             }
@@ -272,24 +245,50 @@ class AppDetailsViewModel @Inject constructor(
         }
     }
 
-    fun getExodusTrackersFromReport(): List<ExodusTracker> {
-        val trackerObjects = exodusReport.value!!.trackers.map {
-            exodusTrackers.getJSONObject(it.toString())
-        }.toList()
+    private fun fetchFavourite(packageName: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _favourite.value = favouriteDao.isFavourite(packageName)
+        }
+    }
 
-        return trackerObjects.map {
-            ExodusTracker(
-                id = it.getInt("id"),
-                name = it.getString("name"),
-                url = it.getString("website"),
-                signature = it.getString("code_signature"),
-                date = it.getString("creation_date"),
-                description = it.getString("description"),
-                networkSignature = it.getString("network_signature"),
-                documentation = listOf(it.getString("documentation")),
-                categories = listOf(it.getString("categories"))
-            )
-        }.toList()
+    private fun fetchDataSafetyReport(packageName: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _dataSafetyReport.value = webDataSafetyHelper.fetch(packageName)
+        }
+    }
+
+    private fun fetchSuggestions() {
+        // Bail out if we go no suggestions to offer
+        if (app.value!!.detailsStreamUrl.isNullOrBlank()) return
+
+        viewModelScope.launch(Dispatchers.IO) {
+            val streamBundle = appDetailsHelper.getDetailsStream(app.value!!.detailsStreamUrl!!)
+            _suggestions.value = streamBundle.streamClusters.values
+                .flatMap { it.clusterAppList }
+                .distinctBy { it.packageName }
+        }
+    }
+
+    private fun fetchExodusPrivacyReport(packageName: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                _exodusReport.value = getLatestExodusReport(packageName)
+            } catch (exception: Exception) {
+                Log.e(TAG, "Failed to fetch privacy report", exception)
+                _exodusReport.value = null
+            }
+        }
+    }
+
+    private fun fetchPlexusReport(packageName: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                _plexusScores.value = getPlexusReport(packageName)?.report?.scores
+            } catch (exception: Exception) {
+                Log.e(TAG, "Failed to fetch compatibility report", exception)
+                _plexusScores.value = null
+            }
+        }
     }
 
     private fun getLatestExodusReport(packageName: String): Report? {
@@ -305,12 +304,6 @@ class AppDetailsViewModel @Inject constructor(
             .firstOrNull()
     }
 
-    private fun getPlexusReport(packageName: String): PlexusReport? {
-        val url = "${Constants.PLEXUS_API_URL}/${packageName}/?scores=true"
-        val playResponse = httpClient.get(url, emptyMap())
-        return json.decodeFromString<PlexusReport>(String(playResponse.responseBytes))
-    }
-
     private fun parseExodusResponse(response: String, packageName: String): List<Report> {
         try {
             val jsonObject = JSONObject(response)
@@ -321,5 +314,11 @@ class AppDetailsViewModel @Inject constructor(
         } catch (e: Exception) {
             return emptyList()
         }
+    }
+
+    private fun getPlexusReport(packageName: String): PlexusReport? {
+        val url = "${Constants.PLEXUS_API_URL}/${packageName}/?scores=true"
+        val playResponse = httpClient.get(url, emptyMap())
+        return json.decodeFromString<PlexusReport>(String(playResponse.responseBytes))
     }
 }