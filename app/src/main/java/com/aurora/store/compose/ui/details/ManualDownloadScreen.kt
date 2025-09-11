07f7c3cccffd3c138cf946ce199d3d913afc2b38
Aayush Gupta <aayushgupta219@gmail.com>
2025-09-11 15:03:46 +0800

    compose: details: Handle imePadding properly during manual download
    AndroidManifest values doesn't works for compose
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/com/aurora/store/compose/ui/details/ManualDownloadScreen.kt
+++ app/src/main/java/com/aurora/store/compose/ui/details/ManualDownloadScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -130,6 +131,7 @@ private fun ScreenContent(
     }
 
     Scaffold(
+        modifier = Modifier.imePadding(),
         topBar = {
             TopAppBarComposable(
                 title = topAppBarTitle,
